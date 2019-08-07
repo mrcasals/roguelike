5cb5f93d8f9947fe12d7c5598dac8a206a28de8e
Marc Riera Casals <mrc2407@gmail.com>
2019-08-07 15:37:27 +0200

    Be able to build walls and ground

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,18 @@ use tcod::console::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
 const LIMIT_FPS: i32 = 20;
 
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -17,6 +27,7 @@ impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
         Object { x, y, char, color }
     }
+
     pub fn move_by(&mut self, dx: i32, dy: i32) {
         self.x += dx;
         self.y += dy;
@@ -28,6 +39,30 @@ impl Object {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
 fn main() {
     let mut root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -35,30 +70,18 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
         .init();
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
-
     tcod::system::set_fps(LIMIT_FPS);
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
     let mut objects = [player, npc];
+    let map = make_map();
 
     while !root.window_closed() {
         con.clear();
 
-        for object in &objects {
-            object.draw(&mut con);
-        }
-
-        blit(
-            &mut con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
+        render_all(&mut root, &mut con, &objects, &map);
 
         root.flush();
 
@@ -92,4 +115,30 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
         _ => {}
     }
     false
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
