01fc47e1193481145b64358a612bae0ccb440473
Marc Riera Casals <mrc2407@gmail.com>
2019-08-07 14:45:30 +0200

    HAndle key presses to move character

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,42 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
         root.set_default_foreground(WHITE);
         root.clear();
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
+
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+}
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Escape, .. } => return true,
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        _ => {}
+    }
+    false
 }
