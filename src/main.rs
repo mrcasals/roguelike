754f760b2aec6105ffdd85d69b56562ac9a25184
Marc Riera Casals <mrc2407@gmail.com>
2019-08-08 15:00:19 +0200

    Add FOW

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::cmp;
 
-use tcod::colors::*;
+use rand::Rng;
+use tcod::colors::{self, Color};
 use tcod::console::*;
 use tcod::map::{FovAlgorithm, Map as FovMap};
-
-use rand::Rng;
 
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -13,6 +12,15 @@ const SCREEN_HEIGHT: i32 = 50;
 // size of the map
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+
+//parameters for dungeon generator
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic; // default FOV algorithm
+const FOV_LIGHT_WALLS: bool = true; // light walls or not
+const TORCH_RADIUS: i32 = 10;
 
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
@@ -33,20 +41,13 @@ const COLOR_LIGHT_GROUND: Color = Color {
     b: 50,
 };
 
-const ROOM_MAX_SIZE: i32 = 10;
-const ROOM_MIN_SIZE: i32 = 6;
-const MAX_ROOMS: i32 = 30;
-
-const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
-const FOV_LIGHT_WALLS: bool = true;
-const TORCH_RADIUS: i32 = 10;
-
 type Map = Vec<Vec<Tile>>;
 
 /// A tile of the map and its properties
 #[derive(Clone, Copy, Debug)]
 struct Tile {
     blocked: bool,
+    explored: bool,
     block_sight: bool,
 }
 
@@ -54,6 +55,7 @@ impl Tile {
     pub fn empty() -> Self {
         Tile {
             blocked: false,
+            explored: false,
             block_sight: false,
         }
     }
@@ -61,6 +63,7 @@ impl Tile {
     pub fn wall() -> Self {
         Tile {
             blocked: true,
+            explored: false,
             block_sight: true,
         }
     }
@@ -92,6 +95,7 @@ impl Rect {
     }
 
     pub fn intersects_with(&self, other: &Rect) -> bool {
+        // returns true if this rectangle intersects with another one
         (self.x1 <= other.x2)
             && (self.x2 >= other.x1)
             && (self.y1 <= other.y2)
@@ -111,7 +115,12 @@ struct Object {
 
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Object { x, y, char, color }
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
     }
 
     /// move by the given amount, if the destination is not blocked
@@ -161,31 +170,52 @@ fn make_map() -> (Map, (i32, i32)) {
     let mut starting_position = (0, 0);
 
     for _ in 0..MAX_ROOMS {
+        // random width and height
         let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
         let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // random position without going out of the boundaries of the map
         let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
         let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
 
         let new_room = Rect::new(x, y, w, h);
+
+        // run through the other rooms and see if they intersect with this one
         let failed = rooms
             .iter()
             .any(|other_room| new_room.intersects_with(other_room));
 
         if !failed {
+            // this means there are no intersections, so this room is valid
+
+            // "paint" it to the map's tiles
             create_room(new_room, &mut map);
+
+            // center coordinates of the new room, will be useful later
             let (new_x, new_y) = new_room.center();
+
             if rooms.is_empty() {
-                starting_position = (new_x, new_y)
+                // this is the first room, where the player starts at
+                starting_position = (new_x, new_y);
             } else {
+                // all rooms after the first:
+                // connect it to the previous room with a tunnel
+
+                // center coordinates of the previous room
                 let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // toss a coin (random bool value -- either true or false)
                 if rand::random() {
+                    // first move horizontally, then vertically
                     create_h_tunnel(prev_x, new_x, prev_y, &mut map);
                     create_v_tunnel(prev_y, new_y, new_x, &mut map);
                 } else {
+                    // first move vertically, then horizontally
                     create_v_tunnel(prev_y, new_y, prev_x, &mut map);
                     create_h_tunnel(prev_x, new_x, new_y, &mut map);
                 }
             }
+
+            // finally, append the new room to the list
             rooms.push(new_room);
         }
     }
@@ -197,27 +227,39 @@ fn render_all(
     root: &mut Root,
     con: &mut Offscreen,
     objects: &[Object],
-    map: &Map,
+    map: &mut Map,
     fov_map: &mut FovMap,
     fov_recompute: bool,
 ) {
     if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
         let player = &objects[0];
         fov_map.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
     }
+
     // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
             let visible = fov_map.is_in_fov(x, y);
             let wall = map[x as usize][y as usize].block_sight;
             let color = match (visible, wall) {
+                // outside of field of view:
                 (false, true) => COLOR_DARK_WALL,
                 (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
                 (true, true) => COLOR_LIGHT_WALL,
                 (true, false) => COLOR_LIGHT_GROUND,
             };
 
-            con.set_char_background(x, y, color, BackgroundFlag::Set);
+            let explored = &mut map[x as usize][y as usize].explored;
+            if visible {
+                // since it's visible, explore it
+                *explored = true;
+            }
+            if *explored {
+                // show explored tiles only (any visible tile is explored already)
+                con.set_char_background(x, y, color, BackgroundFlag::Set);
+            }
         }
     }
 
@@ -272,20 +314,19 @@ fn main() {
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     // generate map (at this point it's not drawn to the screen)
-    let (map, (player_x, player_y)) = make_map();
+    let (mut map, (player_x, player_y)) = make_map();
 
     // create object representing the player
     // place the player inside the first room
-    let player = Object::new(player_x, player_y, '@', WHITE);
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
 
     // create an NPC
-    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
 
     // the list of objects with those two
     let mut objects = [player, npc];
 
-    let mut previous_player_position = (-1, -1);
-
+    // create the FOV map, according to the generated map
     let mut fov_map = FovMap::new(MAP_WIDTH, MAP_HEIGHT);
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
@@ -298,6 +339,9 @@ fn main() {
         }
     }
 
+    // force FOV "recompute" first time through the game loop
+    let mut previous_player_position = (-1, -1);
+
     while !root.window_closed() {
         // clear the screen of the previous frame
         con.clear();
@@ -308,7 +352,7 @@ fn main() {
             &mut root,
             &mut con,
             &objects,
-            &map,
+            &mut map,
             &mut fov_map,
             fov_recompute,
         );
